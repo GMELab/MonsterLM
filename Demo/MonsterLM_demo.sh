90ceb4bd2055d6116ca842e84ec49e1a7450fe71
Matteo Di Scipio <80165657+discipim@users.noreply.github.com>
2023-04-30 14:30:18 -0400

    Update MonsterLM_demo.sh

--- Demo/MonsterLM_demo.sh
+++ Demo/MonsterLM_demo.sh
@@ -4,7 +4,7 @@ BASH_dir="~/MonsterLM/Demo" #set up the Demo directory from the .zip package
 cd ${BASH_dir}
 
 echo -e "
-Scenario: Consider raw values for 'outcome A' and 'exposure A' in 5,000 individuals.\n
+Scenario: Consider unadjusted values for 'outcome A' and 'exposure A' in 5,000 individuals.\n
 Expected adjusted R^2 for Heritability: 0.2.
 Expected adjusted R^2 for GxE: 0.05.\n
 Run MonsterLM to perform the 3 steps outlined in the method overview to get estimates.
@@ -13,7 +13,7 @@ Run MonsterLM to perform the 3 steps outlined in the method overview to get esti
 Rscript ./MonsterLM_demo.R "outcome_A.RData" "exposure_A.RData" "genotype_matrix_sim_3blocks.RData" ${BASH_dir}
 
 echo -e "
-Scenario: Consider raw values for 'outcome B' and 'exposure B' in 5,000 individuals.\n
+Scenario: Consider unadjusted values for 'outcome B' and 'exposure B' in 5,000 individuals.\n
 Expected adjusted R^2 for Heritability: 0.0.
 Expected adjusted R^2 for GxE: 0.0.\n
 Run MonsterLM to perform the 3 steps outlined in the method overview to get estimates.
